80d475eda6d518b443d3800daef4132b275bf795
Franklin Antony <franklinanton.007@gmail.com>
2019-09-20 03:22:31 +0530

    ⚛️ Updated square component

--- src/index.js
+++ src/index.js
@@ -11,17 +11,30 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onclick={() => alert('click')}>
-        {this.props.value}
+      <button className="square"
+        onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
 
   render() {
     const status = 'Next player: X';
